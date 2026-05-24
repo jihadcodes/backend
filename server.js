ad5971e53fbcb428d948847fdd4405e741f4efa3
jihadcodes <jihad.dev.pro@gmail.com>
2026-05-24 23:53:14 +0600

    sample data

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-const express = recuire('express');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send([
+    {
+      name: 'John Doe',
+      age: 30
+    },
+    {
+      name: 'Jane Doe',
+      age: 25
+    }   
+  ]);
+});
+
+app.listen(8000);
